5545df499f60653b252096267f7b8e38bd81ba12
Eduard <>
2023-12-25 13:22:26 +0400

    coco 2017

--- data/get_coco_dataset.sh
+++ data/get_coco_dataset.sh
@@ -10,22 +10,22 @@ mkdir images
 cd images
 
 # Download Images
-wget -c "https://pjreddie.com/media/files/train2014.zip" --header "Referer: pjreddie.com"
-wget -c "https://pjreddie.com/media/files/val2014.zip" --header "Referer: pjreddie.com"
+wget -c "https://pjreddie.com/media/files/train2017.zip" --header "Referer: pjreddie.com"
+wget -c "https://pjreddie.com/media/files/val2017.zip" --header "Referer: pjreddie.com"
 
 # Unzip
-unzip -q train2014.zip
-unzip -q val2014.zip
+unzip -q train2017.zip
+unzip -q val2017.zip
 
 cd ..
 
 # Download COCO Metadata
-wget -c "https://pjreddie.com/media/files/instances_train-val2014.zip" --header "Referer: pjreddie.com"
+wget -c "https://pjreddie.com/media/files/instances_train-val2017.zip" --header "Referer: pjreddie.com"
 wget -c "https://pjreddie.com/media/files/coco/5k.part" --header "Referer: pjreddie.com"
 wget -c "https://pjreddie.com/media/files/coco/trainvalno5k.part" --header "Referer: pjreddie.com"
 wget -c "https://pjreddie.com/media/files/coco/labels.tgz" --header "Referer: pjreddie.com"
 tar xzf labels.tgz
-unzip -q instances_train-val2014.zip
+unzip -q instances_train-val2017.zip
 
 # Set Up Image Lists
 paste <(awk "{print \"$PWD\"}" <5k.part) 5k.part | tr -d '\t' > 5k.txt
